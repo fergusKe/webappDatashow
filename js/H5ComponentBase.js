7f18f8da035e07a6c1dab2b7c1bbfab0e3049ca4
Fergus <fergus.jingtai@gmail.com>
2016-07-01 11:47:30 +0800

    3-7 通用图文组件类-H5ComponentBase（接受 onLoad、onLeave事件）

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -3,10 +3,10 @@ var H5ComponentBase = function(name, cfg) {
 	var cfg = cfg || {};
 	var id = ( 'h5_c_' + Math.random() ).replace('.', '_');
 	// 把當前的組件類型添加到樣式中進行標記
-	var cls = 'h5_component_' + cfg.type + ' h5_component_name_' + name;
+	var cls = 'h5_component_name_' + name + ' h5_component_' + cfg.type;
 	var component = $('<div class="h5_component ' + cls +'" id="' + id + '">');
 	
-	cfg.text   && component.text(cfg.text / 2);
+	cfg.text   && component.text(cfg.text);
 	cfg.width  && component.width(cfg.width / 2);
 	cfg.height && component.height(cfg.height / 2);
 
@@ -19,6 +19,19 @@ var H5ComponentBase = function(name, cfg) {
 			left: '50%'
 		})
 	}
+
+	component.on('onLoad', function() {
+		component.addClass(cls + '_load').removeClass(cls + '_leave');
+		cfg.animateIn && component.animate( cfg.animateIn );
+		console.log(cfg.animateIn);
+    return false;
+  });
+  component.on('onLeave', function() {
+  	component.addClass(cls + '_leave').removeClass(cls + '_load');
+  	cfg.animateOut && component.animate( cfg.animateOut );
+  	console.log(cfg.animateOut);
+    return false;
+  });
 
 	return component;
 }
