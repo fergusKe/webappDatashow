118ff367a71683c5b310ec0aa4be44bfb454036e
Fergus <fergus.jingtai@gmail.com>
2016-07-01 17:58:23 +0800

    3-9 内容组织类-H5（addComponent方法）

--- js/H5.js
+++ js/H5.js
@@ -2,11 +2,12 @@
 var H5 = function() {
 	this.id = ( 'h5_' + Math.random() ).replace('.', '_');
 	this.el = $('<div class="h5" id="' + this.id + '">').hide();
+	this.page = [];
 	$('body').append( this.el );
 
 	// 新增一個頁
 	this.addPage = function(name, text) {
-		var page = $('<div class="h5_page">');
+		var page = $('<div class="h5_page section">');
 
 		if ( name != undefined ) {
 			page.addClass('h5_page_' + name);
@@ -15,16 +16,33 @@ var H5 = function() {
 			page.text(text);
 		}
 		this.el.append(page);
+		this.page.push(page);
 		return this;
 	}
 
 	// 新增一個組件
-	this.addComponent = function() {
+	this.addComponent = function(name, cfg) {
+		var cfg = cfg || {};
+		// 默認type的屬性
+		cfg = $.extend({
+			type: 'base'
+		}, cfg);
+		var component;  // 定義一個變量，存儲組件元素
+		var page = this.page.slice(-1)[0];
+		switch( cfg.type ) {
+			case 'base':
+				component = new H5ComponentBase(name, cfg);
+			break;
 
+			default:
+		}
+		page.append(component);
+		return this;
 	}
 
 	// H5對象初始化呈現
 	this.loader = function() {
+		this.el.fullpage();
 		this.el.show();
 	}
 
